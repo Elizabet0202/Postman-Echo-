38c5139394d52c261cb7c4ea4f2fbbac69c62657
Elizaveta <yelizaveta.baklanova@gmail.com>
2025-11-08 16:32:54 +0300

    Initial commit

---
+++ src/test/java/com/example/test/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package com.example.test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+public class PostmanEchoTest {
+    @Test
+    public void testPostRequestBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data")); // Проверяем, что поле data равно отправленным данным
+    }
+}
+
